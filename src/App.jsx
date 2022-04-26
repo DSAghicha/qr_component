8072ffe94ebddf251aad7de5e2aa8dd759e1d23e
DSAghicha <dsaghicha211@gmail.com>
2022-04-26 17:17:25 +0530

    App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.sass'
+import QrCode from './images/qr-code.png'
 
 const App = () => {
     return (
-        <h1>Hello World</h1>
+        <div className="main">
+            <img src={ QrCode } alt="QR Code" />
+            <h3>Improve your front-end skills by building projects</h3>
+            <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
     )
 }
 
